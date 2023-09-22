fda94983eae879644c27e063c765c89930f93b47
MademVenkateswaraReddy <venkateswara.madem@gmail.com>
2023-09-22 22:26:13 +0530

    fifth commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let addBtn = document.getElementById("add-task")
+let taskInput = document.getElementById("task")
+let taskList = document.getElementById("tasks")
+
+addBtn.addEventListener("click", function(){
+    let task = taskInput.value
+    addTask(task)
+    taskInput.value =""
+})
+function addTask(task){
+    let li = document.createElement("li")
+    li.innerHTML = task
+    taskList.appendChild(li)
+}
